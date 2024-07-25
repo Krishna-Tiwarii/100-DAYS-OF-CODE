32935805c19e250971c237fe1377bea61e80e34f
Krishnaprogrammer101 <139806683+Krishnaprogrammer101@users.noreply.github.com>
2024-07-26 02:28:25 +0530

    Create DAY 6 :-  Peak Index in a Mountain Array.cpp

---
+++ DAY 6 :-  Peak Index in a Mountain Array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+
+        int start=0, end=arr.size()-1 , mid;
+
+        while(start<=end)
+        {
+           mid = end+(start-end)/2 ;    // bcz of overflow issue
+            
+
+            //Peak element
+            if (arr[mid]>arr[mid-1]&& arr[mid]>arr[mid+1])
+             return mid ;
+                    //right side move
+             else if( arr[mid]>arr[mid-1])
+              start = mid+1 ;
+           // left side move
+              else 
+              end = mid-1 ;
+
+        }
+           
+            return -1;
+        }
+};
