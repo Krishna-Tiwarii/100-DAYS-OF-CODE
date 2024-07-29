f296a8c2e9166213cae2a27bc6647f78a316d621
Krishna Tiwari <139806683+Krishna-Tiwarii@users.noreply.github.com>
2024-07-29 21:47:08 +0530

    Create DAY 55 :- Reverse First K element of Queue.cpp

---
+++ DAY 55 :- Reverse First K element of Queue.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+    public:
+    
+    // Function to reverse first k elements of a queue.
+    queue<int> modifyQueue(queue<int> q, int k) {
+        // add code here.
+        stack<int>st;
+        
+         while(k--)
+        {
+            st.push(q.front());
+            q.pop();
+        }
+        
+        int n=q.size();
+        
+        while(!st.empty())
+        {
+            q.push(st.top());
+            st.pop();
+        }
+        
+        while(n--)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+        
+        return q;
+    }
+};
