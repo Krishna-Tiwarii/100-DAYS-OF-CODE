d84ae5779ba32d168a3694f0d38c79933b2fdf03
Krishna Tiwari <139806683+Krishna-Tiwarii@users.noreply.github.com>
2024-08-01 13:28:30 +0530

    Create DAY 57 :- Identical Trees.cpp

---
+++ DAY 57 :- Identical Trees.cpp
@@ -0,0 +1,19 @@
+{
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        //Your Code here
+        if(!r1&&!r2)
+        return 1;
+        
+        if((!r1&&r2 || !r2&&r1))
+        return 0;
+        
+        if(r1->data!=r2->data)
+        return 0;
+    
+    return isIdentical(r1->left,r2->left) && isIdentical(r1->right,r2->right) ;
+    
+    }
+};
