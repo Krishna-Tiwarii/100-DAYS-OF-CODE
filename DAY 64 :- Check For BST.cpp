afe07c49940dfcc2a42fffc29aaf4b5963bd24d8
Krishna Tiwari <139806683+Krishna-Tiwarii@users.noreply.github.com>
2024-08-10 22:57:13 +0530

    Create DAY 64 :- Check For BST.cpp

---
+++ DAY 64 :- Check For BST.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    // Function to check whether a Binary Tree is BST or not.
+    bool BST(Node *root, int &prev)
+    {
+        if(!root)
+        return 1;
+        
+        bool l= BST(root->left , prev);
+        
+        if(l==0)
+        return 0;
+        
+        if(root->data<=prev)
+         return 0;
+        prev = root->data;
+        return BST(root->right ,prev);
+    }
+    bool isBST(Node* root) {
+        // Your code here
+        int prev =INT_MIN ;
+        
+      return  BST(root , prev);
+    }
+};
