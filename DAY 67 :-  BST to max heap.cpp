249c6d55f2ad90d8c2d35d114a83f96a08faee0f
Krishna Tiwari <139806683+Krishna-Tiwarii@users.noreply.github.com>
2024-08-15 18:29:56 +0530

    Create DAY 67 :-  BST to max heap.cpp

---
+++ DAY 67 :-  BST to max heap.cpp
@@ -0,0 +1,33 @@
+class Solution{
+  public:
+  void inorder(Node *root, vector<int>&ans)
+  {
+      if(!root)
+      return;
+      
+      inorder(root->left, ans);
+      ans.push_back(root->data);
+      inorder(root->right, ans);
+}
+
+ void postOrder(Node *root, vector<int>&ans, int &index)
+ {
+     if(!root)
+     return;
+     
+     postOrder(root->left,ans,index);
+     
+     postOrder(root->right,ans,index);
+     root->data = ans[index];
+     index++;
+ }
+    void convertToMaxHeapUtil(Node* root)
+    {
+        // Your code goes here
+        vector<int>ans;
+        inorder(root, ans);
+        
+        int index=0;
+        postOrder(root,ans,index);
+    }    
+};
