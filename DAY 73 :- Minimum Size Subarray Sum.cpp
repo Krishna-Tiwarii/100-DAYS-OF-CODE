b6050aa456700ef59f99dcc619ad213c8ff2f65a
Krishna Tiwari <139806683+Krishna-Tiwarii@users.noreply.github.com>
2024-08-23 16:01:15 +0530

    Create DAY 73 :- Minimum Size Subarray Sum.cpp

---
+++ DAY 73 :- Minimum Size Subarray Sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        
+        int  total= INT_MAX, start =0, end=0 ;
+        int sum=0, n=nums.size();
+
+        while(end<n)
+        {
+            sum+=nums[end]; // add number to the sum
+
+        // window length ko increase  kar sakta hu
+            while(sum>=target)
+            {
+               total = min(total, end-start+1);
+               sum-= nums[start];
+               start++;
+            }
+           // window ki length ko increase karo
+            end++;
+        }
+
+        return total == INT_MAX ? 0 : total;
+    }
+};
